1c6bdfee695953a62ddfb285b983f5c259815f82
Gvrkprasad <glpsk369@gmail.com>
2025-04-01 14:09:00 +0530

    project files updated

--- provider.tf
+++ provider.tf
@@ -18,4 +18,9 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
   }
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
