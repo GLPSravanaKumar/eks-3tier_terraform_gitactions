3a5b41324cdee4ac95d931d16433eb5040ed5b73
Gvrkprasad <glpsk369@gmail.com>
2025-05-12 19:43:53 +0530

    Variables pub/pvt/db_subnet_cidrs type updated

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -91,7 +91,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
 }
 
 resource "aws_eks_node_group" "nodegroup" {
-  cluster_name    = aws_eks_cluster.eks.name
+  cluster_name    = var.cluster_name
   node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = var.pvt_subnet_ids[*].id
--- variables.tf
+++ variables.tf
@@ -4,6 +4,15 @@ variable "database_image" {}
 variable "cluster_name" {}
 variable "region" {}
 variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
-variable "private_subnet_cidr" {}
-variable "database_subnet_cidr" {}
+variable "public_subnet_cidr" {
+    description = "CIDR blocks for public subnets"
+    type        = list(string)
+}
+variable "private_subnet_cidr" {
+    description = "CIDR blocks for public subnets"
+    type        = list(string)
+}
+variable "database_subnet_cidr" {
+    description = "CIDR blocks for public subnets"
+    type        = list(string)
+}
